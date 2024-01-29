41e8611fe385a451ddbc31bfdefda7752e27ad53
SolijonovAsadbek <iamsolijonovasadbek@gmail.com>
2024-01-30 02:23:06 +0500

    30-yanvar 02:23

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from blog.models import Blog
+from itertools import groupby
+from django.utils.timezone import localtime
 
 
 # Create your views here.
 def index(request):
     blogs = Blog.objects.order_by("-created")
 
+    context = {}
+    for key, group in groupby(blogs, lambda post: localtime(post.created).date()):
+        context[key] = list(group)
+
     context = {
-        "blogs": blogs
+        "grouped_blogs": context
     }
 
     return render(request, "blog/index.html", context=context)
