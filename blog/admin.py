e957552d6ff2afaaa8cc00e236f83e08d9357950
SolijonovAsadbek <iamsolijonovasadbek@gmail.com>
2024-01-30 02:55:13 +0500

    30-yanvar 02:55

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,16 @@ from blog.models import Blog
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'image', 'body', 'created', 'updated')
-    list_display_links = ['title', 'body', 'created']
+    list_display = ('title', 'image', 'body_field', 'created', 'updated')
+    list_display_links = ['title', 'created']
     list_filter = ['title']
+
+    def body_field(self, obj):
+        return self._shorten_text(obj.body)
+
+    body_field.short_description = 'body'
+
+    def _shorten_text(self, text, length=10):
+        if len(text) > length:
+            return f'{text[:length]}...'
+        return text
