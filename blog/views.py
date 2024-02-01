c336959643f329580f974b5092bead2cdce2960f
SolijonovAsadbek <iamsolijonovasadbek@gmail.com>
2024-02-01 17:32:34 +0500

    1-fevral 17:320

--- blog/views.py
+++ blog/views.py
@@ -9,12 +9,33 @@ from django.utils.timezone import localtime
 def index(request):
     blogs = Blog.objects.order_by("-created")
 
-    context = {}
-    for key, group in groupby(blogs, lambda post: localtime(post.created).date()):
-        context[key] = list(group)
+    group_year = {}
+    for key, group in groupby(blogs, lambda post: localtime(post.created).year):
+        group_year[key] = list(group)
+
+    month_names = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December",
+    }
+
+    group_month = {}
+    for key, group in groupby(blogs, lambda post: localtime(post.created).month):
+        group_month[month_names.get(key, 'Unknown')] = list(group)
 
     context = {
-        "grouped_blogs": context
+        "grouped_blogs_month": group_month,
+        "grouped_blogs_year": group_year,
+
     }
 
     return render(request, "blog/index.html", context=context)
