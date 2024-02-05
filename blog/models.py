ff5afb7a2c0eb09ed50d37501709eb7f2b0ad7ca
SolijonovAsadbek <iamsolijonovasadbek@gmail.com>
2024-02-05 13:47:07 +0500

    5-fevral 13:47

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,6 @@ class Blog(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title}'
